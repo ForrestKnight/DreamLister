0d1b3a5beb26cec46d1f1e945aa0f034b24af2bc
ForrestKnight <fknig001@odu.edu>
2017-01-23 22:19:20 -0500

    Initial Commit

---
+++ DreamLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Forrest Knight on 1/23/17.
+//  Copyright © 2017 Forrest Knight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
